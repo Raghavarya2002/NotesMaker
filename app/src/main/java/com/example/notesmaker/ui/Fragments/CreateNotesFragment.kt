c891128cd7dfee67fb9008fac6c6aaf71a17cc3f
Raghav <raghavarya2002@gmail.com>
2022-06-23 23:16:15 +0530

    move to the actual coding part

--- app/src/main/java/com/example/notesmaker/ui/Fragments/CreateNotesFragment.kt
+++ app/src/main/java/com/example/notesmaker/ui/Fragments/CreateNotesFragment.kt
@@ -1,11 +1,14 @@
 package com.example.notesmaker.ui.Fragments
 
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.notesmaker.databinding.FragmentCreateNotesBinding
+import java.util.*
 
 
 class CreateNotesFragment : Fragment() {
@@ -19,8 +22,26 @@ class CreateNotesFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentCreateNotesBinding.inflate(layoutInflater, container, false)
 
+        binding.btnSaveNotes.setOnClickListener {
+            createNotes(it)
+        }
+
+
+
         return binding.root
-        //
+
+    }
+
+    private fun createNotes(it: View) {
+        val title = binding.editTitle.text
+        val subtitle = binding.editSubtitle.text
+        val notes = binding.editNotes.text
+
+        val d = Date()
+        val s: CharSequence = DateFormat.format("MMMM d, yyyy ", d.time)
+
+        Log.e("@@@@", "createNotes:  + $s")
+        //1:25:34
     }
 
 
